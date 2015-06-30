2205b8e98f55a1e93f5d6c088e72468e89f8f4e9
GregOSF <gregonza1@MacBook-Bro.local>
2015-06-29 23:07:28 -0700

    Added date var

--- script.js
+++ script.js
@@ -6,21 +6,50 @@ var $newItemsSubmit = $('#newItemsSubmit')
 var $shitToDo = $("#shitToDo")
 var $addedItems = $(".added-Items")
 var $newItemDesc = $("#newItemDesc")
+var $newItemDate = $("#newItemDate")
+var todaysDate = new Date();
+
+
+
+
+
+$.fn.clearForm = function() {
+ return this.each(function() {
+   var type = this.type, tag = this.tagName.toLowerCase();
+   if (tag == 'form')
+     return $(':input',this).clearForm();
+   if (type == 'text' || type == 'password' || tag == 'textarea')
+     this.value = '';
+   else if (type == 'checkbox' || type == 'radio')
+     this.checked = false;
+   else if (tag == 'select')
+     this.selectedIndex = -1;
+ });
+};
 
 $newItemField.on("submit", function(event) {
 	event.preventDefault();
-	$shitToDo.append("<li class='added-Items'>" + "Task: " + $newItems.val() + "<hr>" + "<p>" + "Description: " + $newItemDesc.val() + "</p>" + "</li>" + "<hr>");
+	$shitToDo.append("<li class='added-Items'>" + "Task: " + $newItems.val() + "<hr>" + "<p>" + 
+		"Description: " + $newItemDesc.val() + "</p>" +  "<hr>" + "<p>" + "Due Date: " + 
+		$newItemDate.val() + "</p>" + "</li>" + "<hr>");
+	$('.form-control').clearForm();
 	$("li").on("click", function(event) {
 		event.preventDefault();
-		$(this).wrap("<strike>")
+		$(this).wrap("<strike class='done'>")
 		$(this).fadeOut("slow");
-	});
+	});		var todaysDate = new Date();
+	if ($newItemDate.val() > todaysDate) {
+		$newItemDate.css("color", "red");
+	};
+
+
 	
+
 		
-	});
+
 
 });
-
+});
 
 
 
