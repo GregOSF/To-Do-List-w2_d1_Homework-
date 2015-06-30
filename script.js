1be712209f1575927733c68f8883fc273710848b
GregOSF <gregonza1@MacBook-Bro.local>
2015-06-29 17:56:38 -0700

    Added description form and cross/fade out

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 $(function() {
 
-console.log("working")
+var $newItemField = $("#newItemField")
+var $newItems = $("#newItems")
+var $newItemsSubmit = $('#newItemsSubmit')
+var $shitToDo = $("#shitToDo")
+var $addedItems = $(".added-Items")
+var $newItemDesc = $("#newItemDesc")
+
+$newItemField.on("submit", function(event) {
+	event.preventDefault();
+	$shitToDo.append("<li class='added-Items'>" + "Task: " + $newItems.val() + "<hr>" + "<p>" + "Description: " + $newItemDesc.val() + "</p>" + "</li>" + "<hr>");
+	$("li").on("click", function(event) {
+		event.preventDefault();
+		$(this).wrap("<strike>")
+		$(this).fadeOut("slow");
+	});
+	
+		
+	});
+
+});
 
 
 
@@ -23,4 +42,3 @@ console.log("working")
 
 
 
-}
